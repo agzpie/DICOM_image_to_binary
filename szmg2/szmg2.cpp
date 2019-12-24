fcdde504c2bf85eed17fa44a294693cb41bdf460
Agata Pietralik <agata.pietralik@student.wat.edu.pl>
2019-12-24 15:23:13 +0100

    finished

--- szmg2/szmg2.cpp
+++ szmg2/szmg2.cpp
@@ -1,14 +1,3 @@
-/**
- * @function EqualizeHist_Demo.cpp
- * @brief Demo code for equalizeHist function
- * @author OpenCV team
-**/
-
-//#include "opencv2/imgcodecs.hpp"
-//#include "opencv2/highgui.hpp"
-//#include "opencv2/imgproc.hpp"
-//#include <iostream>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -17,13 +6,39 @@
 using namespace cv;
 using namespace std;
 
-/**
- * @function main
- */
+Mat equalizeIntensity(const Mat& inputImage)
+{
+	if (inputImage.channels() >= 3)
+	{
+		Mat ycrcb;
+
+		imshow("Before", inputImage);
+
+		cvtColor(inputImage, ycrcb, COLOR_BGR2YCrCb);
+
+		vector<Mat> channels;
+
+		split(ycrcb, channels);
+
+		equalizeHist(channels[0], channels[0]);
+
+		Mat result;
+
+		merge(channels, ycrcb);
+
+		cvtColor(ycrcb, result, COLOR_YCrCb2BGR);
+		
+		imshow("YCrCb equalization", ycrcb);
+
+		waitKey();
+	}
+	return Mat();
+}
+
 
  int main(int argc, char** argv)
  {
-	 //! [Load image]
+	 // Loading image
 	 CommandLineParser parser(argc, argv, "{@input | lena.jpg | input image}");
 	 Mat src = imread(samples::findFile(parser.get<String>("@input")), IMREAD_COLOR);
 	 if (src.empty())
@@ -32,44 +47,12 @@ using namespace std;
 		 cout << "Usage: " << argv[0] << " <Input image>" << endl;
 		 return -1;
 	 }
-	 //! [Load image]
 
-	 //! [Convert to grayscale]
-	 cvtColor(src, src, COLOR_BGR2GRAY);
-	 //! [Convert to grayscale]
+	 Mat gray;
 
-	 //! [Apply Histogram Equalization]
-	 Mat dst;
-	 equalizeHist(src, dst);
-	 //! [Apply Histogram Equalization]
+	 equalizeIntensity(src);
 
-	 //! [Display results]
-	 imshow("Source image", src);
-	 imshow("Equalized Image", dst);
-	 //! [Display results]
-
-	 //! [Wait until user exits the program]
 	 waitKey();
-	 //! [Wait until user exits the program]
 
 	 return 0;
-
  }
-
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-//#include <opencv2/imgproc.hpp>
-//#include <iostream>
-//
-//using namespace cv;
-//using namespace std;
-//
-//int main()
-//{
-//	Mat image = Mat::zeros(300, 600, CV_8UC3);
-//	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-//	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
-//	imshow("Display Window", image);
-//	waitKey(0);
-//	return 0;
-//}
